7639158a6762640158df95202524498ad3d1064e
Alexandre Rangel <mamutal91@gmail.com>
2021-06-12 20:32:42 -0300

    beryllium: Initial changes for Kraken
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-kraken
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
